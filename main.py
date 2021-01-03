9ceb283e3007c3a2301952ad40c9df2045253b63
DaniilAnisimov <daniilanisimov2005@yandex.com>
2021-01-04 02:11:45 +0400

    Капучино

--- main.py
+++ main.py
@@ -15,6 +15,11 @@ class MainWindow(QMainWindow):
         self.initUI()
 
     def initUI(self):
+        self.pb_add.clicked.connect(self.add)
+        self.pb_edit.clicked.connect(self.edit)
+        self.load_table()
+
+    def load_table(self):
         result = self.cur.execute("""SELECT * FROM "coffee" """).fetchall()
         title_list = [i[1] for i in self.cur.execute("pragma table_info(coffee)").fetchall()]
         self.tableWidget.setColumnCount(len(title_list))
@@ -24,6 +29,82 @@ class MainWindow(QMainWindow):
             self.tableWidget.setRowCount(i + 1)
             for j, elem1 in enumerate(elem):
                 self.tableWidget.setItem(i, j, QTableWidgetItem(str(elem1)))
+
+    def add(self):
+        dialogue = CoffeeDialog(self.con, self.cur, "add", [])
+        dialogue.show()
+        self.setEnabled(False)
+        dialogue.exec()
+        self.setEnabled(True)
+        self.load_table()
+
+    def edit(self):
+        rows = list(set([i.row() for i in self.tableWidget.selectedItems()]))
+        if len(rows) != 1:
+            return 0
+        select_row = []
+        for i in range(7):
+            select_row.append(self.tableWidget.item(rows[0], i).text())
+        dialogue = CoffeeDialog(self.con, self.cur, "edit", select_row)
+        dialogue.show()
+        self.setEnabled(False)
+        dialogue.exec()
+        self.setEnabled(True)
+        self.load_table()
+
+
+class CoffeeDialog(QDialog):
+    def __init__(self, con, cur, type_d, row):
+        super(CoffeeDialog, self).__init__()
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        if type_d == "add":
+            self.setWindowTitle("Добавить элемент")
+            self.pb_add.setText("Добавить")
+        else:
+            self.setWindowTitle("Редактирование записи")
+            self.pb_add.setText("Сохранить")
+        self.con = con
+        self.cur = cur
+        self.type = type_d
+        self.properties = row
+        self.initUI()
+
+    def initUI(self):
+        self.pb_add.clicked.connect(self.acept_data)
+        if self.type == "edit":
+            self.le_title.setText(self.properties[1])
+            self.le_roasting.setText(self.properties[2])
+            self.le_type.setText(self.properties[3])
+            self.le_taste.setText(self.properties[4])
+            self.le_price.setText(self.properties[5])
+            self.le_volume.setText(self.properties[6])
+
+    def acept_data(self):
+        try:
+            title = self.le_title.text()
+            roasting = self.le_roasting.text()
+            type = self.le_type.text()
+            taste = self.le_taste.text()
+            price = self.le_price.text()
+            volume = self.le_volume.text()
+            if title and roasting and type and taste and price and volume:
+                if self.type == "add":
+                    self.cur.execute("""INSERT INTO coffee("название сорта", "степень обжарки",
+                     "молотый/в зернах", "описание вкуса", "цена", "объем упаковки") VALUES(?, ?, ?, ?, ?, ?)""",
+                                     (title, roasting, type, taste, price, volume))
+                else:
+                    self.cur.execute("""UPDATE coffee SET "название сорта" = ?, "степень обжарки" = ?, 
+                    "молотый/в зернах" = ?, "описание вкуса" = ?, "цена" = ?, "объем упаковки" = ?
+                                      WHERE id = ?""",
+                                     (title, roasting, type, taste, price, volume, self.properties[0]))
+                self.con.commit()
+                self.close()
+            else:
+                self.l_error.setText("Некоторые поля не заполнены")
+        except ValueError:
+            self.l_error.setText("Некорректные значения полей")
+        except sqlite3.IntegrityError:
+            self.l_error.setText("Название занято")
 
 
 def except_hook(cls, exception, traceback):
